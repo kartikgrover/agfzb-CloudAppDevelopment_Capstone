f84e3180c47ec056c24c0349a00e8683818a494b
Kartik <groverkartik2000@yahoo.com>
2023-10-31 23:26:41 -0400

    car make

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
+# Inline class for CarModel to be used within CarMake admin
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 
-# Register your models here.
+# Customizing CarModelAdmin
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'year', 'dealer_id')  # Define the fields to display in the list
+    # Add more customizations as required
 
-# CarModelInline class
+# Customizing CarMakeAdmin with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]  # Embed CarModelInline within CarMake admin
+    list_display = ('name', 'description')  # Fields to display in the list
+    # Add more customizations as needed
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+# Registering models with their respective admins
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
