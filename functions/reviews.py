e8f57cdc5ae2fa0f10812b929eff2b38a8c1dcf3
kartikgrover <groverkartik2000@yahoo.com>
2023-11-07 17:06:15 -0500

    reviews.py fix

--- functions/reviews.py
+++ functions/reviews.py
@@ -2,6 +2,7 @@ from cloudant.client import Cloudant
 from cloudant.query import Query
 from flask import Flask, jsonify, request
 import atexit
+import json
 
 #Add your Cloudant service credentials here
 cloudant_username = '7c2f11c2-7a0f-4fe2-ba2f-f1b6fb4cef82-bluemix'
@@ -64,6 +65,13 @@ def post_review():
             abort(400, description=f'Missing required field: {field}')
 
     # Save the review data as a new document in the Cloudant database
+    print("review--------->", review_data)
+    # Ensure review_data is a dictionary
+    if not isinstance(review_data, dict):
+        review_data = json.loads(review_data)  # Parse string as JSON
+        if not isinstance(review_data, dict):
+            abort(400, description='Invalid JSON data')
+
     db.create_document(review_data)
 
     return jsonify({"message": "Review posted successfully"}), 201
